67616edb128189325d1a32f333de117133fba9e9
Clemo97 <lumumbaclement@gmail.com>
2022-04-11 11:20:05 +0300

    generate new directive

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -1,22 +1,24 @@
 //Input is imported to allow input property binding
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 //Import goal propert class
 import { Goal } from '../goal';
 @Component({
   selector: 'app-goal-detail',
   templateUrl: './goal-detail.component.html',
-  styleUrls: ['./goal-detail.component.css']
+  styleUrls: ['./goal-detail.component.css'],
 })
 export class GoalDetailComponent implements OnInit {
-
   @Input()
   // The "!" after a variable tells TYpescript that the value is not null or undefined.
   goal!: Goal;
 
-  constructor() { }
+  @Output() isComplete = new EventEmitter<boolean>();
 
-  ngOnInit(): void {
+  goalComplete(complete: boolean) {
+    this.isComplete.emit(complete);
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
 
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -24,8 +24,13 @@ export class GoalComponent implements OnInit {
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
 
+  completeGoal(isComplete: any, index: number) {
+    if (isComplete) {
+      this.goals.splice(index, 1);
+    }
+  }
+
   constructor() {}
 
   ngOnInit() {}
-  
 }
